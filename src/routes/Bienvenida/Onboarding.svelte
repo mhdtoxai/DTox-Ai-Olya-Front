6c7e40785eebf33fe5635b90edd5816e290dec31
Mario <mh@dtox.ai>
2024-08-16 11:00:03 -0500

    TestV2

--- src/routes/Bienvenida/Onboarding.svelte
+++ src/routes/Bienvenida/Onboarding.svelte
@@ -190,41 +190,20 @@
         {#if currentQuestionIndex === formData.preguntas.length - 1 && !isSending}
           <div class="flex justify-center mt-4">
             <button
-  type="button"
-  on:click={sendResponsesToAPI}
-  disabled={isSending}
-  class="{isSending ? 'bg-gray-400 cursor-not-allowed' : 'bg-[#32CD32]'} text-white px-8 py-2 rounded flex items-center justify-center"
-  style="min-width: 200px;"
->
-  {#if isSending}
-    <span class="text-white flex items-center space-x-2">
-      <svg
-        class="animate-spin h-5 w-5 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8v8h8a8 8 0 01-16 0z"
-        ></path>
-      </svg>
-      <span>Procesando...</span>
-    </span>
-  {:else}
-    Enviar respuestas
-  {/if}
-</button>
-
+            type="button"
+            on:click={sendResponsesToAPI}
+            disabled={isSending}
+            class="{isSending ? 'bg-white' : 'bg-[#32CD32]'} text-white px-8 py-2 rounded flex items-center justify-center"
+            style="min-width: 200px;"
+          >
+            {#if isSending}
+              <span class="text-white flex items-center space-x-2">
+                <span>Procesando...</span>
+              </span>
+            {:else}
+              Enviar 
+            {/if}
+          </button>
           </div>
         {/if}
       </form>
